8addf47cf1f7ba8317f33430ffbb5d3753bf62e6
M.Uzair <45979852+UzairSamad@users.noreply.github.com>
2019-10-08 16:36:40 +0500

    record of all members

---
+++ Member.js
@@ -0,0 +1,24 @@
+const members = [
+  {
+    id: 1,
+    name: "Ali",
+    status: "Active"
+  },
+  {
+    id: 2,
+    name: "Uzair",
+    status: "Inactive"
+  },
+  {
+    id: 3,
+    name: "Farukh",
+    status: "Active"
+  },
+  {
+    id: 3,
+    name: "Ahmed",
+    status: "Inactive"
+  }
+];
+
+module.exports = members;
